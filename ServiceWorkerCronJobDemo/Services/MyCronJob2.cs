3482e2c51d6736583671cc1ccd85f46e673cf05a
shubhashjha <shubhash.xcode@gmail.com>
2025-04-16 22:02:59 +0530

    Initial commit

---
+++ ServiceWorkerCronJobDemo/Services/MyCronJob2.cs
@@ -0,0 +1,30 @@
+﻿namespace ServiceWorkerCronJobDemo.Services;
+
+public class MyCronJob2 : CronJobService
+{
+    private readonly ILogger<MyCronJob2> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public MyCronJob2(IScheduleConfig<MyCronJob2> config, ILogger<MyCronJob2> logger, IServiceProvider serviceProvider)
+        : base(config.CronExpression, config.TimeZoneInfo, logger)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+        if (config.GetType().GenericTypeArguments[0].Name != GetType().Name)
+        {
+            throw new ArgumentException("Incorrect JobType name for IScheduleConfig.");
+        }
+        if (logger.GetType().GenericTypeArguments[0].Name != GetType().Name)
+        {
+            throw new ArgumentException("Incorrect JobType name for ILogger.");
+        }
+    }
+
+    public override async Task DoWork(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("{now} CronJob 2 is working.", DateTime.Now.ToString("T"));
+        using var scope = _serviceProvider.CreateScope();
+        var svc = scope.ServiceProvider.GetRequiredService<IMyScopedService>();
+        await svc.DoWork(cancellationToken);
+    }
+}
